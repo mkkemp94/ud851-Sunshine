a353e4f70d3922dc2aa41e93a4af46c0d324bd8e
Kemp <kempm@wit.edu>
2017-02-03 20:41:34 -0500

    Call text view from main activity and append a list of fake weather data to it

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,12 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    TextView mWeatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
+        String[] weatherStrings = {"a", "b", "c"};
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String weatherString : weatherStrings) {
+            mWeatherDisplay.append(weatherString + "\n\n\n");
+        }
     }
 }
